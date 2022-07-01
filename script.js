54b26017abae8ed0d264f715e32d2ee28ab8bb08
selvatek <antoine@serin.net>
2022-07-01 16:51:09 +0200

    integration div et span

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-document
+/*document
   .getElementById("addtodo")
   .addEventListener('input', function(event) {
     document
@@ -14,9 +14,29 @@ document
       .getElementById("result")
       .innerHTML = event.target.value;
     
-});
-/*var tag = document.createElement("div, att");
-   var text = document.createTextNode("Tutorix is the best e-learning platform");
-   tag.appendChild(text);
-   var element = document.getElementById("result");
-   element.appendChild(tag); */
+});*/
+
+    // handle the form data
+    var form = document.getElementById("formtodo");
+    document.getElementById("your-id").addEventListener("click", function () {
+      
+     let toDo = document.forms.formtodo.elements.addtodo.value;
+     let toDoType = document.forms.formtodo.elements.type.value;
+      var test = document.createElement("div");
+      test.setAttribute("id","test");
+      var element = document.getElementById("result");
+      element.appendChild(test);
+      var tag = document.createElement("span");
+      var text = document.createTextNode( toDo );
+      tag.appendChild(text);
+      var elements = document.getElementById("test");
+      elements.appendChild(tag); 
+    });
+
+  
+
+
+
+
+
+  
