eebcf9aac1ddf9977e1d70fa1829ead1ebacabe1
selvatek <antoine@serin.net>
2022-07-01 17:22:41 +0200

    p et div fonctionnent

--- script.js
+++ script.js
@@ -17,20 +17,27 @@ document
 });*/
 
     // handle the form data
+    let count = 0;
     var form = document.getElementById("formtodo");
     document.getElementById("your-id").addEventListener("click", function () {
       
      let toDo = document.forms.formtodo.elements.addtodo.value;
      let toDoType = document.forms.formtodo.elements.type.value;
-      var test = document.createElement("div");
-      test.setAttribute("id","test");
+  
+     var container_div = document.getElementById('result');
+     var count = container_div.getElementsByTagName('div').length;
+
+
+     var test = document.createElement("div");
+      test.setAttribute("id","test"+count);
       var element = document.getElementById("result");
       element.appendChild(test);
-      var tag = document.createElement("span");
+      var tag = document.createElement("h2");
       var text = document.createTextNode( toDo );
       tag.appendChild(text);
-      var elements = document.getElementById("test");
+      var elements = document.getElementById("test"+count);
       elements.appendChild(tag); 
+     
     });
 
   
